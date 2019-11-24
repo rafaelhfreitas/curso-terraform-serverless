3326a2647b44f8d80dffe39c37b3daf439630bb4
Rafael Henrique <oraffaudi@gmail.com>
2019-11-24 10:14:18 -0300

    Create first module users on terraform

---
+++ terraform/environments/dev/main.tf
@@ -0,0 +1,7 @@
+module "users" {
+  source         = "../infra/users"
+  environment    = "${var.environment}"
+  write_capacity = 1
+  read_capacity  = 1
+
+}
---
+++ terraform/infra/users/variables.tf
@@ -0,0 +1,13 @@
+variable "environment" {
+
+}
+
+variable "read_capacity" {
+
+}
+
+variable "write_capacity" {
+
+}
+
+
